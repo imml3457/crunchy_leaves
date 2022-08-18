16bef262bdb15c491445963176f7a29805ae4c84
Brandon Kammerdiener <kammerdienerb@MacBook-Pro-7.local>
2022-08-18 15:09:38 -0400

    updated to use mixed-mode attributes

--- crunchy_leaves.c
+++ crunchy_leaves.c
@@ -53,81 +53,81 @@ PACKABLE_STYLE(crunchy_leaves) {
     YED_PLUG_VERSION_CHECK();
 
     tc        = !!yed_get_var("truecolor");
-    attr_kind = tc ? ATTR_RGB : ATTR_256;
+    attr_kind = tc ? ATTR_KIND_RGB : ATTR_KIND_256;
 
     memset(&s, 0, sizeof(s));
 
-    s.active.flags            = attr_kind;
+    s.active.flags            = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BG_KIND_BITS(attr_kind);
     s.active.fg               = foreground;
     s.active.bg               = background;
 
-    s.inactive.flags          = attr_kind;
+    s.inactive.flags          = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BG_KIND_BITS(attr_kind);
     s.inactive.fg             = in_fg;
     s.inactive.bg             = in_bg;
 
     s.active_border           = s.active;
     s.inactive_border         = s.inactive;
 
-    s.cursor_line.flags       = attr_kind;
+    s.cursor_line.flags       = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BG_KIND_BITS(attr_kind);
     s.cursor_line.fg          = text;
     s.cursor_line.bg          = light_grey;
 
-    s.search.flags            = attr_kind | ATTR_INVERSE;
+    s.search.flags            = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BG_KIND_BITS(attr_kind) | ATTR_INVERSE;
     s.search.fg               = srch_fg;
     s.search.bg               = srch_bg;
 
-    s.search_cursor.flags     = attr_kind | ATTR_INVERSE;
+    s.search_cursor.flags     = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BG_KIND_BITS(attr_kind) | ATTR_INVERSE;
     s.search_cursor.fg        = srch_cursor_fg;
     s.search_cursor.bg        = srch_cursor_bg;
 
-    s.selection.flags         = attr_kind;
+    s.selection.flags         = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BG_KIND_BITS(attr_kind);
     s.selection.fg            = text;
     s.selection.bg            = light_grey;
 
-    s.attention.flags         = attr_kind | ATTR_BOLD;
+    s.attention.flags         = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BOLD;
     s.attention.fg            = attn;
 
-    s.associate.flags         = attr_kind | ATTR_BOLD;
+    s.associate.flags         = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BG_KIND_BITS(attr_kind) | ATTR_BOLD;
     s.associate.bg            = assoc_bg;
     s.associate.fg            = assoc_fg;
 
     s.command_line            = s.active;
 
-    s.status_line.flags       = attr_kind | ATTR_BOLD;
+    s.status_line.flags       = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BG_KIND_BITS(attr_kind) | ATTR_BOLD;
     s.status_line.fg          = status_fg;
     s.status_line.bg          = status_bg;
 
     s.active_gutter           = s.active;
     s.inactive_gutter         = s.inactive;
 
-    s.code_comment.flags      = attr_kind | ATTR_BOLD;
+    s.code_comment.flags      = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BOLD;
     s.code_comment.fg         = comment;
 
-    s.code_keyword.flags      = attr_kind | ATTR_BOLD;
+    s.code_keyword.flags      = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BOLD;
     s.code_keyword.fg         = keyword;
 
-    s.code_preprocessor.flags = attr_kind | ATTR_BOLD;
+    s.code_preprocessor.flags = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BOLD;
     s.code_preprocessor.fg    = pp_keyword;
 
-    s.code_control_flow.flags = attr_kind | ATTR_BOLD;
+    s.code_control_flow.flags = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BOLD;
     s.code_control_flow.fg    = control_flow;
 
-    s.code_typename.flags     = attr_kind | ATTR_BOLD;
+    s.code_typename.flags     = ATTR_FG_KIND_BITS(attr_kind) | ATTR_BOLD;
     s.code_typename.fg        = typename;
 
-    s.code_fn_call.flags      = attr_kind;
+    s.code_fn_call.flags      = ATTR_FG_KIND_BITS(attr_kind);
     s.code_fn_call.fg         = call;
 
-    s.code_number.flags       = attr_kind;
+    s.code_number.flags       = ATTR_FG_KIND_BITS(attr_kind);
     s.code_number.fg          = number;
 
-    s.code_constant.flags     = attr_kind;
+    s.code_constant.flags     = ATTR_FG_KIND_BITS(attr_kind);
     s.code_constant.fg        = constant;
 
-    s.code_string.flags       = attr_kind;
+    s.code_string.flags       = ATTR_FG_KIND_BITS(attr_kind);
     s.code_string.fg          = string;
 
-    s.code_character.flags    = attr_kind;
+    s.code_character.flags    = ATTR_FG_KIND_BITS(attr_kind);
     s.code_character.fg       = character;
 
     yed_plugin_set_style(self, "crunchy_leaves", &s);
